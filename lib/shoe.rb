dc02eef2b915410894d6e155a255f6403d84e8ba
Melissa Williams <melissa@brandmeetscopy.com>
2019-05-30 01:17:32 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,10 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+
+    if !(BRANDS.include?(@brand))
+      BRANDS << @brand
+    end
   end
 
   def cobble
@@ -13,10 +17,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
 
-  def brand=(brand)
-    @brand = brand
 
-    BRANDS << brand
+
   end
 
 end
